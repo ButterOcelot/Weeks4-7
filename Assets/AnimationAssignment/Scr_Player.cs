bc1383453b7108167000058f48272a65dfeb48ae
Tyr Rotsaert <rotsaert@shernet.sheridancollege.ca>
2026-02-06 16:14:04 -0500

    worked on animation assignment

---
+++ Assets/AnimationAssignment/Scr_Player.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Scr_Player : MonoBehaviour
+{
+    float horizontal;
+    float speed = 0f;
+    float jumpPower = 14f;
+    bool isFacingRight = true;
+
+    Rigidbody rb;
+    Transform groundCheck;
+    LayerMask groundLayer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = 
+    }
+}
