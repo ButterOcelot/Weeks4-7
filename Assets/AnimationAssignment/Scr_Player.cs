cf547bd14c2b81c9eeeb5a22554b979db9bb7fb9
Tyr Rotsaert <rotsaert@shernet.sheridancollege.ca>
2026-02-06 17:34:54 -0500

    made player move, need to fix ground clipping issue.

--- Assets/AnimationAssignment/Scr_Player.cs
+++ Assets/AnimationAssignment/Scr_Player.cs
@@ -3,23 +3,59 @@ using UnityEngine;
 public class Scr_Player : MonoBehaviour
 {
     float horizontal;
-    float speed = 0f;
-    float jumpPower = 14f;
+    float speed = 8f;
+    float jumpingPower = 14f;
     bool isFacingRight = true;
 
-    Rigidbody rb;
-    Transform groundCheck;
-    LayerMask groundLayer;
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = 
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+        }
+
+        Flip();
     }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = false;
+            Vector3 localeScale = transform.localScale;
+            localeScale.x *= -1f;
+            transform.localScale = localeScale;
+        }
+    }
+
 }
+
+
