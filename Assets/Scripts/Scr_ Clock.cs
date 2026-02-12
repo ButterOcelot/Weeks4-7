c9fee46d196b62848dc84c7479073c2c23cc8975
ButterOcelot <157069905+ButterOcelot@users.noreply.github.com>
2026-02-12 01:28:15 -0500

    could not finish clock challenge
    what it says on the tin

--- Assets/Scripts/Scr_ Clock.cs
+++ Assets/Scripts/Scr_ Clock.cs
@@ -3,8 +3,9 @@ using UnityEngine.InputSystem;
 
 public class Scr_Clock : MonoBehaviour
 {
-
+    private int previousWaitTime;
     public float rotationSpeed;
+    public AudioSource audioSource;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,6 +19,30 @@ public class Scr_Clock : MonoBehaviour
     {
         Vector3 currentRotation = transform.eulerAngles;
         currentRotation.z -= rotationSpeed * Time.deltaTime;
+
+        if((int)currentRotation.z % 45 == 0)
+        {
+            
+            int waitTime = (int)currentRotation.z;
+            bool canPlay = true;
+
+            if (currentRotation.z > previousWaitTime)
+            {
+                canPlay = false;
+            }
+
+            if (canPlay == true)
+            {
+                Debug.Log("This is an hour");
+                audioSource.Play();
+            }
+
+            previousWaitTime = waitTime;
+
+        }
+
+       // Debug.Log(currentRotation.z);
+
         transform.eulerAngles = currentRotation;
     }
 }
