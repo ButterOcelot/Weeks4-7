926775084a6da1acf653f13011618df432a86c7e
butterocelot <tyrcat2005@gmail.com>
2026-02-12 15:03:51 -0500

    added timer

---
+++ Assets/Scripts/Assignment 2/Scr_Timer.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scr_Timer : MonoBehaviour
+{
+    public Slider timerSlider;
+    public float clockDuration;
+
+    private float timeWaiting = 0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        timerSlider.maxValue = clockDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeWaiting += Time.deltaTime * 1;
+        timerSlider.value = timeWaiting;
+
+        if (timeWaiting > clockDuration)
+        {
+            timeWaiting = 0f;
+            Debug.Log("Time is up");
+        }
+
+    }
+}
