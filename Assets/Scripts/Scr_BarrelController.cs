afad1b2891c3d0aadc806907c7dfd07bd730574c
Tyr Rotsaert <rotsaert@shernet.sheridancollege.ca>
2026-02-13 17:02:29 -0500

    First Commit!

---
+++ Assets/Scripts/Scr_BarrelController.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_BarrelController : MonoBehaviour
+{
+
+    public List<Knife> knives = new List<Knife>();
+    public GameObject objToSpawn;
+    public GameObject failStateObj;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public class Knife
+    {
+        
+    }
+
+
+
+}
