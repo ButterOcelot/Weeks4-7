b3a582591bdf2ad44810ef09a752d44799795ec2
ButterOcelot <157069905+ButterOcelot@users.noreply.github.com>
2026-02-19 01:13:48 -0500

    made the topping script parent to the buger when it falls onto it

--- Assets/Assignment/Scripts/Scr_Topping.cs
+++ Assets/Assignment/Scripts/Scr_Topping.cs
@@ -2,19 +2,40 @@ using UnityEngine;
 
 public class Scr_Topping : MonoBehaviour
 {
-
-    public GameObject targetObject;
-    float distance = 10;
+    Vector3 deleteCheck;
+    LayerMask burgerLayer;
+    GameObject burgerObject;
+    Rigidbody2D disableGravity;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        burgerObject = GameObject.Find("Obj_Burger");
+        burgerLayer = LayerMask.GetMask("Burger");
+        disableGravity = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit2D hit = Physics2D
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.001f, burgerLayer);
+
+        if(hit.collider != null)
+        {
+            transform.parent = burgerObject.transform;
+            disableGravity.simulated = false;
+        }
+        else
+        {
+            //Debug.Log("no burger, sad");
+        }
+
+        deleteCheck = transform.position;
+
+        if (deleteCheck.y < -6f)
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
